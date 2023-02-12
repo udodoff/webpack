54510ad465a720565f7debf6cc31766f55847ef1
udodoff <udodovr@gmail.com>
2023-02-12 13:18:16 +0300

    <3

---
+++ src/items.js
@@ -0,0 +1,59 @@
+import image1 from "../media/images/1.jpg"
+import image2 from "../media/images/2.jpg"
+import image3 from "../media/images/3.jpg"
+import image4 from "../media/images/4.gif"
+
+import audio1 from "../media/audio/1.mp3"
+import audio2 from "../media/audio/2.mp3"
+import audio3 from "../media/audio/3.mp3"
+
+import video1 from "../media/video/1.mp4"
+import video2 from "../media/video/2.mp4"
+
+export default [
+    {
+        resource: image1,
+        type: "image",
+        title: "Котик"
+    },
+    {
+        resource: image2,
+        type: "image",
+        title: "Листик"
+    },
+    {
+        resource: image3,
+        type: "image",
+        title: "Выпечка"
+    },
+    {
+        resource: image4,
+        type: "image",
+        title: "Devil noises"
+    },
+    {
+        resource: audio1,
+        type: "audio",
+        title: "Смех"
+    },
+    {
+        resource: audio2,
+        type: "audio",
+        title: "Ку ку"
+    },
+    {
+        resource: audio3,
+        type: "audio",
+        title: "Шуршание"
+    },
+    {
+        resource: video1,
+        type: "video",
+        title: "Парк"
+    },
+    {
+        resource: video2,
+        type: "video",
+        title: "Еще парк"
+    }
+]
